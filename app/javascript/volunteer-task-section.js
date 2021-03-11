d7add1b4a641773fe64c622545556f613dcc4af4
SebastinGoh <39021770+SebastinGoh@users.noreply.github.com>
2021-03-11 15:45:22 +0800

    Started on bootstrap for volunteer mainpage

--- app/javascript/volunteer-task-section.js
+++ app/javascript/volunteer-task-section.js
@@ -2,11 +2,16 @@ function getVolunteerTasks() {
 	return null;
 }
 
+function getNoTaskImage() {
+	return "https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg";
+}
+
 var volunteer_tasks = getVolunteerTasks();
 
 if (volunteer_tasks == null) {
-	document.getElementById("volunteer-task-header").innerHTML = "No tasks available";
+	document.getElementById("volunteer-task-header").innerHTML = "Currently no tasks available";
 	document.getElementById("volunteer-task-list").innerHTML = "<p>We will notify you when there are new requests.</p>";
+	document.getElementById("notaskimage").src = getNoTaskImage(); 
 } else {
 	document.getElementById("volunteer-task-header").innerHTML = "Volunteer Tasks Available";
 	//Table with volunteer tasks
--- app/javascript/welcome.js
+++ app/javascript/welcome.js
@@ -2,5 +2,19 @@ function getName() {
 	return "User";
 }
 
-var welcome_message = 'Welcome ' + getName();
+function getProfile() {
+	return "https://thumbs.dreamstime.com/b/default-avatar-photo-placeholder-profile-icon-eps-file-easy-to-edit-default-avatar-photo-placeholder-profile-icon-124557887.jpg";
+}
+
+function getLogo() {
+	return "https://4m4you.com/wp-content/uploads/2020/06/logo-placeholder.png";
+}
+
+
+	
+var welcome_message = 'Welcome, ' + getName();
 document.getElementById("welcome").innerHTML = welcome_message;
+
+document.getElementById("profile").src = getProfile(); 
+
+document.getElementById("logo").src = getLogo();
