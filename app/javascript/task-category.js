ab8e6d7304f6bd0923c4bfafdb8d5a700e273e4b
SebastinGoh <39021770+SebastinGoh@users.noreply.github.com>
2021-03-14 15:51:28 +0800

    Changes to Carousal

--- app/javascript/task-category.js
+++ app/javascript/task-category.js
@@ -1,7 +1,7 @@
 function getTaskCategory() {
 	//Check page number when getting task category
 	//If got category then return as array [(photo url, name),(photo, name),(photo, name)]
-	var result = [["https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg","Name 1"],["https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg","Name 2"],["https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg","Name 3"],["https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg","Name 4"]]
+	var result = [["https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg","Name 1"],["https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg","Name 2"],["https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg","Name 3"],["https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg","Name 4"],["https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg","Name 5"],["https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg","Name 6"],["https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg","Name 7"],["https://www.foot.com/wp-content/uploads/2017/06/placeholder-square.jpg","Name 8"]]
 	return result;
 }
 
@@ -13,30 +13,42 @@ if (task_category == null) {
 	//No categories to show
 	document.getElementById("task-category").innerHTML = "Nothing";
 } else {
-	var first_category = true;
+	var first_page = true;
+	var cat_count = 0
 	for (let category of task_category) {
+		var page = document.createElement("div");
+		page.setAttribute("id", "category-page");
+		page.className = "carousel-item card row";
+
 		var tile = document.createElement("div");
-		tile.setAttribute("id", "category-tile");
-		tile.className = "carousel-item";
-		if (first_category) {
-			tile.classList.add("active");
-			first_category = false;
-		}
+		tile.className = "card col";
+
 		var tile_image = document.createElement("img");
 		tile_image.src = category[0];
-		tile_image.alt = "slide";
-		tile_image.className = "category-image d-block";
+		tile_image.className = "category-image d-block card-img-top";
 		tile.appendChild(tile_image);
 
 		var tile_words = document.createElement("div");
-		tile_words.className = "category-name carousel-caption";
+		tile_words.className = "category-name card-body";
 		var tile_name = document.createElement("h2")
+		tile_name.className = "card-text";
 		tile_name.innerHTML = category[1];
 		tile_words.appendChild(tile_name);
 		tile.appendChild(tile_words);
 
-		var element = document.getElementById("task-category");
-		element.appendChild(tile);
+		page.appendChild(tile);
+		tile = undefined;
+		cat_count++;
+
+		if (cat_count%4 === 0) {
+			if (first_page) {
+				page.classList.add("active");
+				first_page = false;
+			}
+			var element = document.getElementById("task-category");
+			element.appendChild(page);
+			page = undefined;
+		}
 	}
 	//Tiles with task categories
 	//var page_num = 'Page' + getPageNum();
